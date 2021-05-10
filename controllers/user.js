6de52bf89d02751099fa049a8e29aa118a01770c
Adeyemi juwon <samju7778@gmail.com>
2021-05-10 14:24:30 +0100

    portfolio

--- controllers/user.js
+++ controllers/user.js
@@ -213,13 +213,13 @@ const signin = async (req, res) => {
 //     res.status(500).json({ message: 'something went wrong' });
 //   }
 // };
-// const getPortfolioData = async (req, res) => {
-//   try {
-//     const portfolioData = await Portfolio.find({});
-//     res.status(200).json({ message: ' successful', portfolioData });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'server error' });
-//   }
-// };
-module.exports = { signup, signin };
+const getPortfolioData = async (req, res) => {
+  try {
+    const portfolioData = await Portfolio.find({});
+    res.status(200).json({ message: ' successful', portfolioData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'server error' });
+  }
+};
+module.exports = { signup, signin, getPortfolioData };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 // let User = require('../models/user');
-const { signup, signin } = require('../controllers/user');
+const { signup, signin, getPortfolioData } = require('../controllers/user');
 
 // router.route('/').get((req, res) => {
 //   User.find()
@@ -12,6 +12,6 @@ router.route('/signup').post(signup);
 router.route('/signin').post(signin);
 // router.route('/google').post(google);
 // router.route('/facebook').post(facebook);
-// router.route('/portfolio').get(getPortfolioData);
+router.route('/portfolio').get(getPortfolioData);
 
 module.exports = router;
