ccd6b89a52df52e4a29c3e3b8434e93813d4495c
Adeyemi juwon <samju7778@gmail.com>
2021-05-10 13:52:54 +0100

    remove message

--- controllers/message.js
+++ controllers/message.js
@@ -1,27 +1,27 @@
-let Message = require('../models/message');
-const getFeedback = async (req, res) => {
-  try {
-    const feedback = await Message.find({});
-    res.status(200).json({ feedback });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'server error' });
-  }
-};
-const feedback = async (req, res) => {
-  const { name, email, message } = req.body;
-  try {
-    const feedback = await Message.create({
-      name,
-      email,
-      message,
-    });
-    res
-      .status(200)
-      .json({ feedback, message: 'Your feedback successfully sent' });
-  } catch (error) {
-    res.status(500).json({ message: 'something went wrong' });
-  }
-};
+// let Message = require('../models/message');
+// const getFeedback = async (req, res) => {
+//   try {
+//     const feedback = await Message.find({});
+//     res.status(200).json({ feedback });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'server error' });
+//   }
+// };
+// const feedback = async (req, res) => {
+//   const { name, email, message } = req.body;
+//   try {
+//     const feedback = await Message.create({
+//       name,
+//       email,
+//       message,
+//     });
+//     res
+//       .status(200)
+//       .json({ feedback, message: 'Your feedback successfully sent' });
+//   } catch (error) {
+//     res.status(500).json({ message: 'something went wrong' });
+//   }
+// };
 
-module.exports = { feedback, getFeedback };
+// module.exports = { feedback, getFeedback };
