d1c6a95c50394a894fe1f7640301f465c8bd16a7
Adeyemi juwon <samju7778@gmail.com>
2021-10-13 12:01:15 +0100

    subscribe

--- app.js
+++ app.js
@@ -15,9 +15,11 @@ app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
 const usersRouter = require('./routes/users');
 const messageRouter = require('./routes/message');
 const productRoutes = require('./routes/productRoutes');
+const subscribeRoutes = require('./routes/subscribe');
 // app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 app.use('/', messageRouter);
+app.use('/', subscribeRoutes);
 app.use('/api/products', productRoutes);
 
 // app.listen(port, () => {
---
+++ models/subscribe.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const subscribeSchema = new Schema(
+  {
+    email: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Subscribe = mongoose.model('Subscribe', subscribeSchema);
+
+module.exports = Subscribe;
