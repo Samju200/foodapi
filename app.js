7a125adc49783139585f364ea65894aef1a21e2b
samju200 <samju7778@gmail.com>
2023-07-05 09:23:11 +0100

    reform db

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-const connectionDb = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const connectionDb = require("./config/db");
 // const transporter = require('./config/mailer');
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-connectionDb();
-app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+// connectionDb();
+// app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
 
 // const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/users');
-const messageRouter = require('./routes/message');
-const productRoutes = require('./routes/productRoutes');
-const subscribeRoutes = require('./routes/subscribe');
+const usersRouter = require("./routes/users");
+const messageRouter = require("./routes/message");
+const productRoutes = require("./routes/productRoutes");
+const subscribeRoutes = require("./routes/subscribe");
 // app.use('/exercises', exercisesRouter);
-app.use('/users', usersRouter);
-app.use('/', messageRouter);
-app.use('/', subscribeRoutes);
-app.use('/api/products', productRoutes);
+app.use("/users", usersRouter);
+app.use("/", messageRouter);
+app.use("/", subscribeRoutes);
+app.use("/api/products", productRoutes);
 
 // app.listen(port, () => {
 //   console.log(`Server is running on port: ${port}`);
 // });
+connectionDb().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
--- config/db.js
+++ config/db.js
@@ -1,20 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+// const app = express()
+// const PORT = process.env.PORT || 3000
 
-const connectionDb = () => {
+// const connectionDb = () => {
+//   try {
+//     const uri = process.env.CONNENCTION_URL;
+//     mongoose.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     const connection = mongoose.connection;
+//     connection.once('open', () => {
+//       console.log('MongoDB database connection established successfully');
+//     });
+//   } catch (error) {
+//     console.log('MongoDB database connection unsuccessfully');
+//   }
+// };
+const connectionDb = async () => {
   try {
-    const uri = process.env.CONNENCTION_URL;
-    mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const connection = mongoose.connection;
-    connection.once('open', () => {
-      console.log('MongoDB database connection established successfully');
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log('MongoDB database connection unsuccessfully');
+    console.log(error);
+    process.exit(1);
   }
 };
 
